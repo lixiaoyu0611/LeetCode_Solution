a259d4ef9e5b9b2cac0af4de654dc13a5296d798
Xiaoyu Li <1211026012@qq.com>
2018-10-08 22:42:58 +0800

    ADD TEST

--- test.cpp
+++ test.cpp
@@ -1,32 +1,77 @@
 #include<iostream>
 using namespace std;
-class Solution
+const int MAX = 501;
+int road[MAX][MAX];
+int rescue[MAX];
+bool visit[501] = {false};
+int n,m;
+int starting,ending;
+int len[MAX];
+int main()
 {
-public:
-    int strStr(string haystack, string needle)
+    cin >> n >> m >> starting >> ending;
+    for (int i = 0; i < n; i++)
+        cin >> rescue[i];
+    for (int i = 0; i < n; i++)
     {
-        int m = haystack.length();
-        int n = needle.length();
-        if (!n)
+        for (int j = 0; j < n; j++)
         {
-            return 0;
+            if (i == j)
+                road[i][j] = 0;
+            else
+                road[i][j] = MAX;
         }
-        for (int i = 0; i < m - n + 1; i++)
-        {
-            int j = 0;
-            for (; j < n; j++)
-            {
-                if (haystack[i + j] != needle[j])
-                {
-                    break;
-                }
-            }
-            if (j == n)
-            {
-                return i;
-            }
-        }
-        return -1;
-
     }
-};
+    for (int i = 0; i < n; i++)
+        len[i] = MAX;
+    int c1,c2;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> c1 >> c2;
+        cin >> road[c1][c2];
+        road[c2][c1] = road[c1][c2];
+        if (c1 == starting)
+            len[c2] = road[c1][c2];
+        if (c2 == starting)
+            len[c1] = road[c1][c2];
+    }
+    int MIN,index;
+    int dis[MAX] = {0};
+    dis[starting] = 1;
+    len[starting] = 0;
+    int maxrecure[MAX] = {0};
+    maxrecure[starting] = rescue[starting];
+
+    while(1)
+    {
+        MIN = MAX;
+        for (int i = 0; i < n; i++)
+        {
+            if (!visit[i] && len[i] < MIN)
+            {
+                MIN = len[i];
+                index = i;
+            }
+        }
+
+        visit[index] = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (!visit[i] && road[index][i] != MAX && MIN + road[index][i] < len[i])
+            {
+                len[i] = MIN + road[index][i];
+                dis[i] = dis[index];
+                maxrecure[i] = maxrecure[index] + rescue[i];
+            }
+            else if (!visit[i] && road[index][i] != MAX &&  MIN + road[index][i] == len[i])
+            {
+                dis[i] += dis[index];
+                maxrecure[i] = max(maxrecure[i], maxrecure[index] + rescue[i]);
+            }
+        }
+        if (index == ending)
+            break;
+    }
+    cout<<dis[ending]<<" "<<maxrecure[ending];
+}
